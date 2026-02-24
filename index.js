dc993bb788396db34b6939dc52848354c2c87e28
MIN Sovansokheng <min.sovansokheng@ababank.com>
2026-02-24 14:24:08 +0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.json({ message: 'Mock API is running!' });
+});
+
+app.get('/hello', (req, res) => {
+  res.json({ greeting: 'Hello from the mock API!' });
+});
+
+app.listen(port, () => {
+  console.log(`Mock API listening at http://localhost:${port}`);
+});
