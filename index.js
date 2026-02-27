6e7e80c0ab41335517894ed27f55698b92863a95
MIN Sovansokheng <min.sovansokheng@ababank.com>
2026-02-27 16:46:45 +0700

    test

--- index.js
+++ index.js
@@ -16,5 +16,23 @@ app.listen(port, () => {
   console.log(`Mock API listening at http://localhost:${port}`);
 });
 
+const awsAccessKey = "AKIAIOSFODNN7EXAMPLE";
+const awsSecretKey = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY";
 
+const githubToken = "ghp_1234567890abcdefghijklmnopqrstuvwxyzABCD";
+
+const openaiKey = "sk-1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHI";
+
+const slackToken = "xoxb-123456789012-123456789012-abcdefghijklmnopqrstuvwxyzABCD";
+
+const privateKey = `
+-----BEGIN RSA PRIVATE KEY-----
+MIIEpAIBAAKCAQEA1FakePrivateKeyForTestingOnlyDoNotUse1234567890
+abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890
+-----END RSA PRIVATE KEY-----
+`;
+
+const randomSecret = "p9Zx7LmQ2rT8vW4yHk3Jn6Ub0Cd5Ef1Gh8Ij2Kl9Mn0Op3Qr4St6Uv7Wx8Yz";
+
+//testing purposes only
 
